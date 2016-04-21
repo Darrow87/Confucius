7b1026b717ba7d0954b5ace26e89286bab176b98
Patrick Skelley & john seo <pair+pskelley.seodo@devbootcamp.com>
2016-04-21 16:59:58 -0400

    add error handling

--- db/migrate/20160420131623_create_answers.rb
+++ db/migrate/20160420131623_create_answers.rb
@@ -1,10 +1,14 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :content
+      t.string :content, null: false
       t.references :user
       t.references :question
+<<<<<<< 0e5645787b186a3c5782869e648ac1b72576a67f
       t.boolean :best_answer, default: false
+=======
+      t.boolean :best_answer { default: false }
+>>>>>>> add error handling
 
       t.timestamps null: false
     end
