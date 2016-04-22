57730357376131c6f4a180e0e60cd36aa78a42e9
Patrick Skelley & john seo <pair+pskelley.seodo@devbootcamp.com>
2016-04-21 20:18:07 -0400

    add some ajax for answering quenstions

--- public/js/application.js
+++ public/js/application.js
@@ -14,7 +14,21 @@ $(document).ready(function() {
 //       url: $target.attr("action")
 //     })
 
+// $('#answer-form').on('submit', function(event){
+//   event.preventDefault();
+//   $target = $(event.target)
+//   $.ajax({
+//     method: $target.attr("method"),
+//     url: $target.attr("action"),
+//     data: $target.serialize()
+//   }).done(function(response){
+//     $('#answer_container').append(response)
+//   })
 
 
-//   });
-// });
+})
+
+
+
+
+});
