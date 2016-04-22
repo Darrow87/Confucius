eb25cb6406ef04bbb63719b99825d089aca884ab
Patrick Skelley & john seo <pair+pskelley.seodo@devbootcamp.com>
2016-04-21 21:39:50 -0400

    ajax partial for new answer form functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
 //   $('.vote-button').on('submit', function(event){
 //     event.preventDefault();
@@ -14,19 +10,19 @@ $(document).ready(function() {
 //       url: $target.attr("action")
 //     })
 
-$('#answer-form').on('submit', function(event){
-  event.preventDefault();
-  $target = $(event.target)
-  $.ajax({
-    method: $target.attr("method"),
-    url: $target.attr("action"),
-    data: $target.serialize()
-  }).done(function(response){
-    $('#answer_container').append(response)
+  $('#answer-form').on('submit', function(event){
+    event.preventDefault();
+    $target = $(event.target)
+    $.ajax({
+      method: $target.attr("method"),
+      url: $target.attr("action"),
+      data: $target.serialize()
+    }).done(function(response){
+      $('#answer_container').append(response);
+      $target.trigger("reset");
+    })
   })
 
-
-})
 
 
 
